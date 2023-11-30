6cc755f080668a01d1830e264c4ac19fcb43b486
Dmytro Zelenetskyi <dmitry.zelenetskiy@gmail.com>
2023-11-30 22:58:50 +0100

    update

--- src/languages/graphql.ts
+++ src/languages/graphql.ts
@@ -2,8 +2,9 @@ import { scope } from "../utils.js";
 import tokens from "../generated/tokens.json";
 
 export default [
-  scope(["keyword.type.graphql", "support.type.builtin.graphql"], tokens.syntax.keyword.fg),
+  scope(["keyword.type.graphql", "support.type.builtin.graphql", "keyword.fragment.graphql", "keyword.on.graphql"], tokens.syntax.keyword.fg),
   scope("variable.graphql", tokens.syntax.property.fg),
   scope(["punctuation.operation.graphql", "punctuation.colon.graphql"], tokens.syntax.punctuation.fg),
   scope(["variable.parameter.graphql"], tokens.syntax.parameter.fg, "italic"),
+  scope("entity.name.fragment.graphql", tokens.syntax.variable.fg),
 ];
